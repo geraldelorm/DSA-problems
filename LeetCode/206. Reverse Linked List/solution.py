0a6bf3bcc21379a1eb873755e2076ba85a251beb
Gerald Elorm Gbagbe <gerald.elorm.gbagbe@gmail.com>
2022-06-02 01:03:07 +0000

    easy Linked List questions

---
+++ LeetCode/206. Reverse Linked List/solution.py
@@ -0,0 +1,41 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+
+# Iterative Solution
+from typing import Optional
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prevNode, currNode = None, head
+        while currNode:
+            nextNode = currNode.next
+            currNode.next = prevNode
+            prevNode = currNode
+            currNode = nextNode
+        return prevNode
+
+# Time Complexity = O(n)
+# Space Complexity = O(1)
+    
+# Recursive Solution
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        if not head:
+            return None
+        
+        newHead = head
+        if head.next:
+            newHead  = self.reverseList(head.next)
+            head.next.next = head
+        head.next = None
+        
+        return newHead
+
+# Time Complexity = O(n)
+# Space Complexity = O(1)
