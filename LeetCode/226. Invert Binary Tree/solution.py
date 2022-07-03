3decf160296c626129e2e2d479f271407f25fcf0
Gerald Elorm Gbagbe <gerald.elorm.gbagbe@gmail.com>
2022-07-03 13:39:08 +0000

    easy BST Traversal questions

---
+++ LeetCode/226. Invert Binary Tree/solution.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def helper(node):
+            if not node:
+                return 
+            node.right, node.left = node.left, node.right
+            helper(node.right)
+            helper(node.left)
+            
+        helper(root)
+        return root
+
+
+# Time O(n)
+# Space O(1)
