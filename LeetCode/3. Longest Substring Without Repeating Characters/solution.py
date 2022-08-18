e1ea6e29f154a1ed34797e58141dd156bdb9759a
Gerald Gbagbe <gerald.elorm.gbagbe@gmail.com>
2022-08-18 18:20:09 +0000

    question 3 solution with an array as substring

--- LeetCode/3. Longest Substring Without Repeating Characters/solution.py
+++ LeetCode/3. Longest Substring Without Repeating Characters/solution.py
@@ -1,16 +1,14 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        left, right, length, maxLength = 0, 0, 0, 0
+        left, right, length = 0, 0, 0
         
         while right < len(s):
             if s[right] in s[left:right]:
-                left = left + 1
-                length -= 1
+                left += 1
             else:
-                length += 1
                 right += 1
-                maxLength = max(length, maxLength)
-        return maxLength
+                length = max(length, right - left)
+        return length
 
 # Time O(n) or possibly n^2 our substring s[left: right] could take O(n) to search 
 # Space O(1)
