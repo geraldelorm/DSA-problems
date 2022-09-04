a8567cfe8e215d9f95acc9cc80035c629d8a3a08
Gerald Gbagbe <gerald.elorm.gbagbe@gmail.com>
2022-09-04 00:39:03 +0000

    easy blind 75

--- LeetCode/20. Valid Parentheses/solution.py
+++ LeetCode/20. Valid Parentheses/solution.py
@@ -17,3 +17,26 @@ class Solution:
 
 # Time Complexity = O(n)
 # Space Complexity = O(n)
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = deque()
+
+        for c in s:
+            if c in ["(", "{", "["]:
+                stack.appendleft(c)
+            elif len(stack) < 1:
+                return False
+            else:
+                if c == ")" and stack.popleft() != "(":
+                    return False
+                if c == "]" and stack.popleft() != "[":
+                    return False
+                if c == "}" and stack.popleft() != "{":
+                    return False
+
+        return len(stack) == 0
+
+#   Time O(n)
+#   Space O(n)
