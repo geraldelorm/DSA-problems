c5975645d0eff10939b59c14775986c2155cc67c
Gerald Gbagbe <gerald.elorm.gbagbe@gmail.com>
2022-09-13 21:20:22 +0000

    day 7 1WPK

--- LeetCode/121. Best Time to Buy and Sell Stock/solution.py
+++ LeetCode/121. Best Time to Buy and Sell Stock/solution.py
@@ -1,14 +1,33 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        max_profit = 0
-        
-        for i, b in enumerate(prices):
-            for j, s in enumerate(prices):
-                if j > i:
-                    max_profit = max(max_profit, s - b)
-        
-        return max_profit
-    
-#     Time = O(n ^ 2)
-#     Space = (1)
+        maxProfit = 0
+        b, s = 0, len(prices) - 1
 
+        for i in range(len(prices)):
+            for j in range(len(prices)):
+                if j > i:
+                    currProfit = prices[j] - prices[i]
+                    maxProfit = max(maxProfit, currProfit)
+
+        return maxProfit
+
+#     Time = O(n^2)
+#     Space = O(1)
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxProfit = 0
+        left, right = 0, 1  # buy sell
+
+        while right < len(prices):
+            if prices[left] < prices[right]:
+                currProfit = prices[right] - prices[left]
+                maxProfit = max(maxProfit, currProfit)
+            else:
+                left = right
+            right += 1
+
+        return maxProfit
+
+# Time = O(n)
+# Space = O(1)
