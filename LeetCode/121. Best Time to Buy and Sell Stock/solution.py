73a6a8ca89a195b9313f2e89a069f666016c128b
Gerald Gbagbe <gerald.elorm.gbagbe@gmail.com>
2022-09-13 21:25:37 +0000

    two pointer problem buy/sell stocks

--- LeetCode/121. Best Time to Buy and Sell Stock/solution.py
+++ LeetCode/121. Best Time to Buy and Sell Stock/solution.py
@@ -1,15 +1,15 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        maxProfit = 0
-        b, s = 0, len(prices) - 1
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         maxProfit = 0
+#         b, s = 0, len(prices) - 1
 
-        for i in range(len(prices)):
-            for j in range(len(prices)):
-                if j > i:
-                    currProfit = prices[j] - prices[i]
-                    maxProfit = max(maxProfit, currProfit)
+#         for i in range(len(prices)):
+#             for j in range(i + 1, len(prices)):
+#                 if prices[j] > prices[i]:
+#                     currProfit = prices[j] - prices[i]
+#                     maxProfit = max(maxProfit, currProfit)
 
-        return maxProfit
+#         return maxProfit
 
 #     Time = O(n^2)
 #     Space = O(1)
