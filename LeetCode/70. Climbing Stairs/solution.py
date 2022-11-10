f588d7ed9e727cd33a52320eb917de0f0f7cc05c
Gerald Gbagbe <gerald.elorm.gbagbe@gmail.com>
2022-11-10 21:02:44 +0000

    dp easy

--- LeetCode/70. Climbing Stairs/solution.py
+++ LeetCode/70. Climbing Stairs/solution.py
@@ -22,3 +22,18 @@ class Solution:
 
 #     Time = O(n)
 #     Space = O(1)
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+
+        dp = [0, 1, 2]
+
+        for i in range(3, n + 1):
+            dp.append(dp[i - 1] + dp[i - 2])
+
+        return dp[n]
