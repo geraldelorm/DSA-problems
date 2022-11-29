5313b7564cb196682fb5de99a474cea56938d940
Gerald E. Gbagbe <gerald.elorm.gbagbe@gmail.com>
2022-11-29 17:13:07 +0000

    Delete LeetCode directory

--- LeetCode/1636. Sort Array by Increasing Frequency/solution.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def frequencySort(self, nums: List[int]) -> List[int]:
-        nums.sort(reverse=True)
-        d=sorted(nums,key=nums.count)
-        
-        return d 
-
-# TIme O(nlogn)
-# Space O(n)
