e21c7ccb30cddd1b5a286365b23e264c029247df
Gerald Elorm Gbagbe <gerald.elorm.gbagbe@gmail.com>
2022-12-17 18:50:50 +0000

    easy B

--- Others/B-E/496. Next Greater Element I.py
+++ Others/B-E/496. Next Greater Element I.py
@@ -1,9 +1,6 @@
 # The next greater element of some element x in an array is the first greater element that is to the right of x in the same array.
-
 # You are given two distinct 0-indexed integer arrays nums1 and nums2, where nums1 is a subset of nums2.
-
 # For each 0 <= i < nums1.length, find the index j such that nums1[i] == nums2[j] and determine the next greater element of nums2[j] in nums2. If there is no next greater element, then the answer for this query is -1.
-
 # Return an array ans of length nums1.length such that ans[i] is the next greater element as described above.
 
 # Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
--- Others/B-E/605. Can Place Flowers.py
+++ Others/B-E/605. Can Place Flowers.py
@@ -12,30 +12,44 @@
 
 # Input: flowerbed = [1,0], n = 1
 # Output: false
-
 class Solution:
-    def isValidSpot(self, i, N, flowerbed):
-        if i == 0 and i < N - 1:
-            return flowerbed[i + 1] == 0
-        elif i == N - 1:
-            return flowerbed[i - 1] == 0
-        elif i < N -1 and i > 0:
-            return flowerbed[i + 1] == 0 and flowerbed[i - 1] == 0 
-        else:
-            return True
-            
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        N = len(flowerbed) 
-            
-        for i in range(N):
-            if n == 0:
-                return True
-            
-            elif flowerbed[i] == 0 and self.isValidSpot(i, N, flowerbed):
+        flowerbed = [0] + flowerbed + [0]
+
+        for i in range(1, len(flowerbed) - 1): #ignore the first and last added
+            if flowerbed[i - 1] == 0 and flowerbed[i] == 0 and flowerbed[i + 1] == 0:
                 flowerbed[i] = 1
                 n -= 1
 
-        return n == 0
+        return n <= 0
+
+# Time = O(N)
+# Space = O(N) is modification to flowerbed is consitered
+
+
+# class Solution:
+#     def isValidSpot(self, i, N, flowerbed):
+#         if i == 0 and i < N - 1:
+#             return flowerbed[i + 1] == 0
+#         elif i == N - 1:
+#             return flowerbed[i - 1] == 0
+#         elif i < N -1 and i > 0:
+#             return flowerbed[i + 1] == 0 and flowerbed[i - 1] == 0 
+#         else:
+#             return True
+            
+#     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+#         N = len(flowerbed) 
+            
+#         for i in range(N):
+#             if n == 0:
+#                 return True
+            
+#             elif flowerbed[i] == 0 and self.isValidSpot(i, N, flowerbed):
+#                 flowerbed[i] = 1
+#                 n -= 1
+
+#         return n == 0
     
-#  Space = O(1)
-#  Time = O(N)
+# #  Space = O(1)
+# #  Time = O(N)
