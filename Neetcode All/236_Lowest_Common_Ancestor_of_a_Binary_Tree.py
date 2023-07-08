b2e381c45c65f41f616c6bf288523ca3d8f7f1e6
Gerald E. Gbagbe <gerald.elorm.gbagbe@gmail.com>
2023-07-08 04:43:07 +0000

    blind 75

--- Neetcode All/236_Lowest_Common_Ancestor_of_a_Binary_Tree.py
+++ Neetcode All/236_Lowest_Common_Ancestor_of_a_Binary_Tree.py
@@ -7,12 +7,17 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # if tree is empty or root val is either p or q val return the root ~ we found the LCA
         if root == None or root == p or root == q: return root
+        # recursively find LCA in right and left subtree
         left = self.lowestCommonAncestor(root.left, p, q)
         right = self.lowestCommonAncestor(root.right, p, q)
+        
+# at end of recursion ei at the leaves return the root if values was found in both left & right
+# else return the non none node
         if left != None and right != None: return root
         if left != None: return left
-        return right
+        if right != None: return right
     
 # Time = O(n)
-# Space = O(h) ~ height of tree ~ recursive call stack
+# Space = O(n)
