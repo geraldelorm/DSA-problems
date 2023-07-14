3f4bff1fdc3a8c3094be89f358d62de49436eaf5
Gerald E. Gbagbe <gerald.elorm.gbagbe@gmail.com>
2023-07-14 23:59:25 +0000

    blind 75

---
+++ Blind 75/133_Clone_Graph.py
@@ -0,0 +1,31 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        oldToNew = {}
+        
+        def clone(node):
+            if node in oldToNew:
+                return oldToNew[node]
+            
+            copy = Node(node.val)
+            oldToNew[node] = copy
+            
+            for nei in node.neighbors:
+                copy.neighbors.append(clone(nei))
+                
+            return copy
+        
+        if not node: 
+            return None 
+        else: 
+            return clone(node)
+        
+    # Time = O(n + m) vertices + edges
+    # Space = O(N)
