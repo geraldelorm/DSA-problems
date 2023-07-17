1c542cd610a85ba5be1aec26ec1d1bee675fb8e2
Gerald E. Gbagbe <gerald.elorm.gbagbe@gmail.com>
2023-07-17 20:13:24 +0000

    blind 75

---
+++ Blind 75/55_Jump_Game.py
@@ -0,0 +1,13 @@
+#Greedy 
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        goal = len(nums) - 1
+        
+        for i in range(len(nums) - 2, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i  
+            
+        return goal == 0 
+        
+# Time = O(n)
+        
