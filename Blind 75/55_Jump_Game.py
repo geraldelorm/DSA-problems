1656937f6b365455344b43f5c91f69c13f2f773e
Gerald E. Gbagbe <gerald.elorm.gbagbe@gmail.com>
2023-07-18 02:34:55 +0000

    blind 75

--- Blind 75/55_Jump_Game.py
+++ Blind 75/55_Jump_Game.py
@@ -1,3 +1,30 @@
+#DP 
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        
+        def canJumpFromPos(nums, pos, memo):
+            if pos in memo:
+                return memo[pos]
+            
+            if pos == len(nums) - 1:
+                memo[pos] = True
+                return True
+            
+            furthestJump = min(pos + nums[pos], len(nums) - 1)
+            
+            for i in range(pos + 1, furthestJump + 1):
+                if canJumpFromPos(nums, i, memo):
+                    memo[pos] = True
+                    return True
+                
+            memo[pos] = False
+            return False
+        
+        return canJumpFromPos(nums, 0, {})
+# Time = O(n^2)
+# Space = O(n) recursion and memo
+        
+    
 #Greedy 
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
@@ -10,4 +37,4 @@ class Solution:
         return goal == 0 
         
 # Time = O(n)
-        
+
