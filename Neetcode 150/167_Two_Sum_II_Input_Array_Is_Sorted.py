2cc95651edf85e671cf6e03668be10e9b058b112
Gerald E. Gbagbe <gerald.elorm.gbagbe@gmail.com>
2023-08-08 17:55:29 +0000

    neetcode 150

--- Neetcode 150/167_Two_Sum_II_Input_Array_Is_Sorted.py
+++ Neetcode 150/167_Two_Sum_II_Input_Array_Is_Sorted.py
@@ -1,16 +1,38 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        for i, v in enumerate(numbers):
+            for j in range(i + 1, len(numbers)):
+                if v + numbers[j] == target:
+                    return [i +1, j + 1]
+    # TC: O(n^2)
+    # SC: O(1)
+
+            
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, v in enumerate(numbers):
+            diff = target - v
+            if diff in seen:
+                return [seen[diff] + 1, i + 1]
+            seen[v] = i
+
+    # TC: O(n)
+    # SC: O(n)
+    
+    
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
         left, right = 0, len(numbers) - 1
         
         while left < right:
-            sum = numbers[left] + numbers[right]
-            if sum == target:
+            curr_sum = numbers[left] + numbers[right]
+            if curr_sum == target:
                 return [left + 1, right + 1]
-            elif sum < target:
-                left += 1
-            else:
+            if curr_sum > target:
                 right -= 1
-          
-        
-# Time = O(n)
-# Space = O(1)
+            else:
+                left += 1
+
+    # TC: O(n)
+    # SC: O(1)
