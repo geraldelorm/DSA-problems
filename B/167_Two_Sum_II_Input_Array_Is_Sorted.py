2333c4f14322fdde83b7cf2f34116d749ecaf7e2
Gerald E. Gbagbe <gerald.elorm.gbagbe@gmail.com>
2023-08-17 02:44:34 +0000

    B

---
+++ B/167_Two_Sum_II_Input_Array_Is_Sorted.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left, right = 0, len(numbers) - 1
+
+        while left < right:
+            sum = numbers[left] + numbers[right]
+
+            if sum > target:
+                right -= 1
+            elif sum < target:
+                left += 1
+            else:
+                return [left + 1, right + 1]
+
+            # TC:O(n)
+            # SC: O(1)
