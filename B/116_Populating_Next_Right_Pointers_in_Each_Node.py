70c6953a7ce41db151451204078f897ddb173a10
Gerald E. Gbagbe <gerald.elorm.gbagbe@gmail.com>
2023-09-04 23:29:42 +0000

    B

---
+++ B/116_Populating_Next_Right_Pointers_in_Each_Node.py
@@ -0,0 +1,33 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root: return root
+
+        queue = collections.deque()
+        queue.append(root)
+
+        while queue:
+            curr_level_size = len(queue)
+
+            for i in range(curr_level_size):
+                node = queue.popleft()
+                if i < curr_level_size - 1:
+                    node.next = queue[0]
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+        return root
+
+        # TC: O(n)
+        # SC: O(n)
