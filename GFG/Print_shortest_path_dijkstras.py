f41bb41700ca3450b7a679c88ca705af0130c2fb
geraldelorm <gerald.elorm.gbagbe@gmail.com>
2024-11-08 20:44:09 +0000

    prit shortest path, dijkstra's

--- GFG/Print_shortest_path_dijkstras.py
+++ GFG/Print_shortest_path_dijkstras.py
@@ -10,38 +10,36 @@ class Solution:
         for node, next_node, cost in edges:
             graph[node].append([next_node, cost])
             graph[next_node].append([node, cost])
-        
+
         distance = [float("inf")] * (n + 1)
         parent = [-1] * (n + 1)
-            
+
         p_queue = []
         heapq.heappush(p_queue, (0, 1, -1))
-        
+
         while p_queue:
             curr_dist, curr_node, parent_node = heapq.heappop(p_queue)
-            
+
             if curr_dist > distance[curr_node]:
                 continue
-            
+
             distance[curr_node] = curr_dist
             parent[curr_node] = parent_node
-            
+
             for next_node, next_dist in graph[curr_node]:
                 if curr_dist + next_dist < distance[next_node]:
                     distance[next_node] = curr_dist + next_dist
                     heapq.heappush(p_queue, (curr_dist + next_dist, next_node, curr_node))
 
-        
         if distance[n] == float("inf"): return [-1]
-        
+
         path = []
         node = n
-        
+
         while node != -1:
             path.append(node)
             node = parent[node]
-            
+
         path.append(distance[n])
         path.reverse()
         return path
-        
